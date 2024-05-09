5548a147cedbdea1af95d294e94d7b344c9b3cc5
joaovitor1004 <108817899+joaovitor1004@users.noreply.github.com>
2024-05-09 08:48:33 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -9,16 +9,32 @@ botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho(){
     if(tamanhoSenha > 1){
-        tamanhoSenha = tamanhoSenha-1;
+        //tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
-
 function aumentaTamanho(){
     if(tamanhoSenha < 20){
-        tamanhoSenha = tamanhoSenha+1;
+        //tamanhoSenha = tamanhoSenha+1;
+        tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
-console.log(botoes);
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+geraSenha();
+
+function geraSenha(){
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha;i++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+}
